4a6bab443fb844972b8fc2565f4e24c32919d448
rrc_f2024_abansal2 <abansal2@rrc.ca>
2025-10-01 19:03:31 -0500

    Did basic setup

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(morgan("combined"));
+
+// Default root route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// Routes
+//@ts-ignore
+app.get("/health", (req, res) => {
+    res.json({
+        status: 200,
+        text: "Server is healthy"});
+});
+
+export default app;
