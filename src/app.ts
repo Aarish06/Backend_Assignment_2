38db60605c6ea13a90fc23ec73dd77f09a007bb0
rrc_f2024_abansal2 <abansal2@rrc.ca>
2025-11-18 15:00:09 -0600

    Implemented helmet.js

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,13 @@ import morgan from "morgan";
 import employeeRoutes from "./api/v1/routes/employeeRoutes";
 import branchRoutes from "./api/v1/routes/branchRoutes";
 import dotenv from "dotenv";
-
+import helmet from "helmet";
 const app = express();
 dotenv.config();
 // Middleware
 app.use(express.json());
 app.use(morgan("combined"));
+app.use(helmet());
 
 // Default root route
 app.get("/", (req, res) => {
