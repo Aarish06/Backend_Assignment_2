5460bfa342c7475ae2a2fa969a1b968215f959d0
rrc_f2024_abansal2 <abansal2@rrc.ca>
2025-11-18 14:40:57 -0600

    Enviroment Variable Setup

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import express from "express";
 import morgan from "morgan";
 import employeeRoutes from "./api/v1/routes/employeeRoutes";
 import branchRoutes from "./api/v1/routes/branchRoutes";
+import dotenv from "dotenv";
 
 const app = express();
-
+dotenv.config();
 // Middleware
 app.use(express.json());
 app.use(morgan("combined"));
